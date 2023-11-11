1ac6d0ebe2c07b919156d2483528a3e09c875f55
White Tree <whitetree@Whitetreecodiinggalaxy.local>
2023-11-11 22:57:34 +0800

    tom_binary_tree_jdbc_mysql

---
+++ JDBCDemo/src/main/java/com/tom/app/App.java
@@ -0,0 +1,59 @@
+package com.tom.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Database connection parameters
+        String url = "jdbc:mysql://localhost:3306/seller_token_api";
+        String user = "root";
+        String password = "root";
+
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish a connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            // If the connection is successful
+            if (conn != null) {
+                System.out.println("Connected to the database successfully!");
+
+                // Create a statement
+                Statement stmt = conn.createStatement();
+
+                // Execute a query
+                ResultSet rs = stmt.executeQuery("SELECT * FROM user");
+
+                // Process the result set
+                while (rs.next()) {
+                    // Get metadata
+                    java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+                    int columnCount = rsmd.getColumnCount();
+
+                    // The column count starts from 1
+                    for (int i = 1; i <= columnCount; i++ ) {
+                        String name = rsmd.getColumnName(i);
+                        // Print column name and value
+                        System.out.println(name + ": " + rs.getString(i));
+                    }
+                }
+            } 
+            else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load JDBC driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Could not connect to the database: " + e.getMessage());
+        }
+    }
+}
