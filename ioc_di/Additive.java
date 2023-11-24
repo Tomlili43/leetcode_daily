72ebdd0da5d15a69b9ef63b0ce494b55206c82bf
White Tree <whitetree@Whitetreecodiinggalaxy.local>
2023-11-24 13:44:47 +0800

    tom_ioc_di

---
+++ ioc_di/Additive.java
@@ -0,0 +1,12 @@
+package ioc_di;
+// https://www.cnblogs.com/tanghaorong/p/13364634.html
+/**
+ * @desc Additive class
+ * @author tom du
+ */
+
+public class Additive {
+    public void addAdditive(){
+        System.out.println("add additive");
+    }
+}
---
+++ ioc_di/OrangeJuice.java
@@ -0,0 +1,12 @@
+package ioc_di;
+
+/**
+ * @author tom
+ * @desc 橙汁类
+ */
+public class OrangeJuice {
+    public void needOrangeJuice(){
+        System.out.println("consumer order a cup of orange juice");
+    }
+}
+
